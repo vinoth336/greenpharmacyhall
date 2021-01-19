685b573130d4670b74970b9fbca57eeac2222967
vinothkumar <vinoth336@gmail.com>
2021-01-19 19:09:49 +0530

    Added uuid for user creation

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'sex', 'city_id', 'state_id', 'zipcode', 'phone_no',
+        'id', 'name', 'email', 'password', 'sex', 'city_id', 'state_id', 'zipcode', 'phone_no',
         'address'
     ];
 
