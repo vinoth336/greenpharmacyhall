5b4c3ca0a6cddee7137e4fa02f74a9d8a2c8ab5d
vinothkumar <vinoth336@gmail.com>
2021-02-06 19:42:31 +0530

    Added missed file

---
+++ resources/views/product/import_image.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app', ['activePage' => 'Product Images', 'titlePage' => __('Create Product')])
+
+@section('content')
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <form method="post" action="{{ route('product.import_image') }}" autocomplete="off" class="form-horizontal" enctype="multipart/form-data">
+                        @csrf
+                        @method('post')
+
+                        <div class="card ">
+                            <div class="card-header card-header-primary">
+                                <h4 class="card-title">{{ __('Import Product Image') }}</h4>
+                            </div>
+                            <div class="card-body ">
+                                @if (session('status'))
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="alert alert-success">
+                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                    <i class="material-icons">close</i>
+                                                </button>
+                                                <span>{{ session('status') }}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="text-center">
+                                            <label class="col-form-label">{{ __('Import Image') }}</label>
+                                            <input name="product_images[]"  type="file" multiple />
+                                            @if ($errors->has('product_list'))
+                                                <span id="product_list-error" class="error text-danger"
+                                                    for="input-product_list">{{ $errors->first('product_list') }}</span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer ml-auto mr-auto">
+                                <a href="{{ route('product.index') }}" class="btn btn-info">{{ __('Cancel') }}</a>
+                                <button type="submit" class="btn btn-primary">{{ __('Import') }}</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
