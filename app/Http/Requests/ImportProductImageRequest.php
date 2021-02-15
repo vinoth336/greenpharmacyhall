8d5573014500120f7b3077374626b57c1f13cecc
vinothkumar <vinoth336@gmail.com>
2021-02-15 23:17:31 +0530

    remove max upload limit

--- app/Http/Requests/ImportProductImageRequest.php
+++ app/Http/Requests/ImportProductImageRequest.php
@@ -24,8 +24,8 @@ class ImportProductImageRequest extends FormRequest
     public function rules()
     {
         return [
-            'product_images.*' => 'image|mimes:png,jpg,jpeg|max:6048',
-            'product_images' => 'max:20'
+            'product_images.*' => 'image|max:6048',
+            'product_images' => 'max:2000'
         ];
     }
 }
