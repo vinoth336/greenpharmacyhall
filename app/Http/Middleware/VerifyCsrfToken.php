de3826c7d14c814ab155cbed97f918b9156f85fa
vinothkumar <vinoth336@gmail.com>
2024-05-26 00:28:34 +0530

    fix: removed testing code

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -24,16 +24,13 @@ class VerifyCsrfToken extends Middleware
             $this->inExceptArray($request) ||
             $this->tokensMatch($request)
         ) {
-            info("am inside");
             return tap($next($request), function ($response) use ($request) {
                 if ($this->shouldAddXsrfTokenCookie()) {
-                    info("am inside 2");
                     $this->addCookieToResponse($request, $response);
                 }
             });
         }
 
-        dd(request()->all(), "am inside");
 	return redirect()->route("public.login")->with('message', 'Page Expired');
         // throw new TokenMismatchException('CSRF token mismatch.');
     }
